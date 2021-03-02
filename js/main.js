442acf5d96cc9c4e2e3f683460c2e8add2429c03
cloudpc7 <cloudp7@hotmail.com>
2021-03-01 22:42:34 -0800

    final

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 /*----- constants -----*/
 const color = {
-    '0': 'white',
+    'null': 'white',
     '1': 'blue',
     '-1': 'black'
 }
-const chickenDinner = [
+const winCombo = [
     [0,1,2],
-    [0,3,6],
-    [6,7,8],
     [0,3,6],
     [1,4,7],
     [2,5,8],
@@ -15,33 +13,51 @@ const chickenDinner = [
     [6,7,8],
     [0,4,8],
     [2,4,6]
-
 ]
-const x = 'x';
-const o = 'o';
 /*----- app's state (variables) -----*/
 let board, winner, turn;
 /*----- cached element references -----*/
 const boardSpace = document.getElementById('boardSpace')
+msgEl = document.getElementById('msg');
 /*----- event listeners -----*/
 boardSpace.addEventListener('click',dropBox);
+document.querySelector('button').addEventListener('click', init);
 /*----- functions -----*/
 init();
+
+function dropBox(e){
+    const index = parseInt(e.target.id);
+    if (board[index] || winner) return;
+    board[index] = turn;
+    turn *= -1;
+    winner = getWinner();
+    render();
+}
+
+function getWinner(){
+    for(let i = 0; i < winCombo.length;i++){
+       if (Math.abs(board[winCombo[i][0]] + board[winCombo[i][1]] + board[winCombo[i][2]]) === 3) return board[winCombo[i][0]];
+       if (board.includes(null)) return null;
+       return 'T';
+    };
+}
+function render() {
+    board.forEach(function(box, index){
+        document.getElementById(index).style.backgroundColor = color[box];
+    if (winner === 'T'){
+        msgEl.textContent = "It's a Tie!";
+    } else if (winner){
+        msgEl.innerHTML = `<span style="color": ${color[winner]}">${color[winner].toUpperCase()}</span> Wins!`;
+    } else {
+        msgEl.innerHTML = `<span style="color: ${color[turn]}">${color[turn].toUpperCase()}</span>'s Turn`;
+    }
+    })
+}
+
 function init() {
-    board = [0,0,0,0,0,0,0,0,0];
+    board = [null,null,null,null,null,null,null,null,null];
     turn = 1;
     winner = null;
     render();
 }
 
-function render() {
-    board.forEach(function(box, index){
-        document.getElementById(box).style.backgroundColor = color[box];
-    })
-}
-render();
-function dropBox(e){
-
-    turn *= -1;
-    render();
-}
